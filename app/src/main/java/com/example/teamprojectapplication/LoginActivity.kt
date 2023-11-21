9a44f48a961ffe9298d01221c687321f8d275b77
choyoungseo20 <whdudtj0809@naver.com>
2023-11-21 22:30:56 +0900

    회원가입/로그인 구현

---
+++ app/src/main/java/com/example/teamprojectapplication/LoginActivity.kt
@@ -0,0 +1,60 @@
+package com.example.teamprojectapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.teamprojectapplication.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class LoginActivity : AppCompatActivity() {
+    private var auth: FirebaseAuth? = null
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnLogin.setOnClickListener {
+            signinAndSignup()
+        }
+    }
+
+    private fun signinAndSignup() {
+        auth?.createUserWithEmailAndPassword(
+            binding.txtEmail.text.toString(),
+            binding.txtPassword.text.toString()
+        )?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                moveMainPage(task.result?.user)
+            } else if (task.exception?.message.isNullOrEmpty()) {
+                Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+            } else {
+                signinEmail()
+            }
+        }
+    }
+
+    private fun signinEmail() {
+        auth?.signInWithEmailAndPassword(
+            binding.txtEmail.text.toString(),
+            binding.txtPassword.text.toString()
+        )?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                moveMainPage(task.result?.user)
+            } else {
+                Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}

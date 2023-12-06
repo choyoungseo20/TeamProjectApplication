1ef1115ef804d909cbb34220dc83583fe2db3301
choyoungseo20 <whdudtj0809@naver.com>
2023-12-07 05:17:26 +0900

    코드 정리

--- app/src/main/java/com/example/teamprojectapplication/AddDiaryFragment.kt
+++ app/src/main/java/com/example/teamprojectapplication/AddDiaryFragment.kt
@@ -11,29 +11,23 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.teamprojectapplication.databinding.FragmentAddDiaryBinding
 import com.example.teamprojectapplication.viewmodel.postViewModel
-import java.text.SimpleDateFormat
-import java.util.Date
 import android.content.Intent
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import com.google.firebase.storage.FirebaseStorage
+import androidx.lifecycle.Observer
 
 class AddDiaryFragment : Fragment() {
-
-    var binding : FragmentAddDiaryBinding? = null
+    private var binding : FragmentAddDiaryBinding? = null
     val viewModel: postViewModel by activityViewModels()
 
-
-
     var photoUri : Uri? = null
-    var photoResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-        if(it.resultCode == RESULT_OK){
+    val photoResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){//photoResult.launch(intent) 비동기 -> 콜백함수
+        if( it.resultCode == RESULT_OK ) {
             photoUri = it.data?.data
-
+            binding?.imgArea?.setImageURI(photoUri)
+            viewModel.imageUpload(photoUri)
         }
     }
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -50,39 +44,17 @@ class AddDiaryFragment : Fragment() {
 
         }
 
-        binding?.imgArea?.setOnClickListener {
+        viewModel.imageUrl.observe(viewLifecycleOwner, Observer { imageUrl ->
+            imageUrl?.let {
+                Toast.makeText(requireActivity(), "업로드 성공: $imageUrl", Toast.LENGTH_LONG).show()
+            }
+        })
 
+        binding?.imgArea?.setOnClickListener {//apply
             val intent = Intent(Intent.ACTION_PICK)
             intent.type = "image/*"
             photoResult.launch(intent)
-
-
-
-            /*viewModel.setOnPhotoSelectedListener { uri ->
-                binding?.imgArea?.setImageURI(uri)
-            }
-
-             */
-
-            /*val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-            registerForActivityResult.launch(intent)
-
-             */
         }
-
-        binding?.btnUpload?.setOnClickListener {
-            binding?.imgArea?.setImageURI(photoUri)
-            imageUpload()
-        }
-
-        /*
-        binding?.btnUpload?.setOnClickListener {
-            Log.d("AddDiary", "URI value: $uri") // Log the URI value
-            viewModel.imageUpload(uri)
-            //imageUpload(uri)
-        }
-
-         */
 
         binding?.btnSkip?.setOnClickListener {
             viewModel.setPost()
@@ -95,87 +67,8 @@ class AddDiaryFragment : Fragment() {
             findNavController().navigate(R.id.action_addDiaryFragment_to_homeFragment)
         }
     }
-
-    /*
-    private val registerForActivityResult =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            when (result.resultCode) {
-                AppCompatActivity.RESULT_OK -> {
-                    val uri: Uri? = result.data?.data
-                    uri?.let {
-                        binding?.imgArea?.setImageURI(uri)
-                    }
-                }
-            }
-        }
-
-     */
-
-    private fun imageUpload() {
-        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val imageFileName = "IMAGE ${timestamp}.png"
-
-        val storage = FirebaseStorage.getInstance()
-        val storageRef = storage.reference.child("image")
-
-        val storagePath = storageRef.child(imageFileName)
-
-        photoUri?.let { uri ->
-            storagePath.putFile(uri).continueWithTask {
-                return@continueWithTask storagePath.downloadUrl
-            }.addOnCompleteListener { downloadUrl ->
-                viewModel.setImageUrl(downloadUrl.result.toString())
-                Toast.makeText(requireActivity(), "업로드 성공 : ${downloadUrl.result}", Toast.LENGTH_LONG).show()
-
-            }
-        }
-
-
-
-
-
-
-
-    }
-
-    /*private fun imageUpload(uri: Uri) {
-        val storage = FirebaseStorage.getInstance()
-        // storage 참조
-        val storageRef = storage.reference.child("image")
-        // storage에 저장할 파일명 선언
-        val fileName = SimpleDateFormat("yyyyMMddHHmmss").format(Date())
-        val mountainsRef = storageRef.child("${fileName}.png")
-        val uploadTask = mountainsRef.putFile(uri)
-        uploadTask.addOnSuccessListener { taskSnapshot ->
-            // 파일 업로드 성공
-            Toast.makeText(requireActivity(), "사진 업로드 성공", Toast.LENGTH_SHORT).show()
-        }.addOnFailureListener {
-            // 파일 업로드 실패
-            Toast.makeText(requireActivity(), "사진 업로드 실패", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-     */
-
-    /*
-    private fun imageDownload() {
-        val downloadTask = mountainsRef.downloadUrl
-        downloadTask.addOnSuccessListener { uri ->
-            // 파일 다운로드 성공
-            // Glide를 사용하여 이미지를 ImageView에 직접 가져오기
-            Glide.with(requireContext()).load(uri).into(binding?.imgArea)
-        }.addOnFailureListener {
-            // 파일 다운로드 실패
-        }
-    }
-
-     */
-
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
     }
-
-
-
 }
